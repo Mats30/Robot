be70ebc7e12424259b037f06f1ae09ddbe8eda68
Mats <mati.rataj@gmail.com>
2018-02-23 22:51:48 +0100

    Changes in database module reverted, local deploy script removed

--- database/src/main/java/com/scrappy/database/controllers/BookController.java
+++ database/src/main/java/com/scrappy/database/controllers/BookController.java
@@ -4,12 +4,14 @@ import com.scrappy.database.dto.BookDTO;
 import com.scrappy.database.services.BookService;
 import com.scrappy.database.services.BookServiceQualifier;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.List;	 
 
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/books")
 public class BookController {
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mvn clean install
-mv database/target/database-1.0-SNAPSHOT.war /opt/tomcat/apache-tomcat-9.0.5/webapps/
-cd /opt/tomcat/apache-tomcat-9.0.5/
-./bin/startup.sh
