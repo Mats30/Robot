df1a0bfebc8b03274e9684e50c513c165dd775fd
Krzychu <szczepanskikrs@gmail.com>
2018-02-28 09:48:26 +0100

    Added: * Cargo plugin. * Cargo plugin config. * 1.0 RC version update.

--- robot/src/app/services/book.service.ts
+++ robot/src/app/services/book.service.ts
@@ -6,19 +6,20 @@ import {isUndefined} from "util";
 
 @Injectable()
 export class BookService {
-  private host: string = "http://147.135.210.145:8080/database-1.0-SNAPSHOT/books";
+  private host: string = "147.135.210.145:8080/database-1.0-RC/books";
 
   constructor(private http: HttpClient) {
   }
 
-  public getPagedBooksList(page: number, numberInPage: number, sort: string): Observable<BooksApi> {
+  public getPagedBooksList(page: number, numberInPage: number, sort: string, direction: string): Observable<BooksApi> {
     if (isUndefined(numberInPage) || isUndefined(sort)) {
       numberInPage = 5;
       sort = "title";
+      direction = "asc";
     }
 
     const requestUrlParams =
-      `?page=${page}&size=${numberInPage}&sort=${sort}`;
+      `?page=${page}&size=${numberInPage}&sort=${sort},${direction}`;
 
     return this.http.get<BooksApi>(this.host + "/all" + requestUrlParams);
   }
